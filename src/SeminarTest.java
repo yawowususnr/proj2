63e71299b778ae162e810d97b7c256907886b3b6
chrisnicoue <144281691+chrisnicoue@users.noreply.github.com>
2024-10-02 22:29:22 -0400

    Update SeminarTest.java

--- src/SeminarTest.java
+++ src/SeminarTest.java
@@ -2,10 +2,10 @@ import student.TestCase;
 
 // -------------------------------------------------------------------------
 /**
- *  Test the Seminar class
+ * Test the Seminar class
  *
- *  @author CS3114/CS5040 staff
- *  @version July 2023, last updated September 2023
+ * @author CS3114/CS5040 staff
+ * @version July 2023, last updated September 2023
  */
 public class SeminarTest extends TestCase {
     /**
@@ -19,9 +19,8 @@ public class SeminarTest extends TestCase {
     /**
      * Check the toString method
      */
-    public void testtoString()
-    {
-        String[] keywords = {"Good", "Bad", "Ugly"};
+    public void testtoString() {
+        String[] keywords = { "Good", "Bad", "Ugly" };
         String expected = "ID: 1729, Title: Seminar Title\n"
             + "Date: 2405231000, Length: 75, X: 15, Y: 33, Cost: 125\n"
             + "Description: This is a great seminar\n"
@@ -45,21 +44,20 @@ public class SeminarTest extends TestCase {
 
 
     /**
-     * Check the serialization/deserialization process
+     * Check the serialization/deserialization process ehllo
+     * 
      * @throws Exception
      */
-    public void testSeminarDS()
-        throws Exception
-    {
-        String[] keywords = {"Good", "Bad", "Ugly"};
+    public void testSeminarDS() throws Exception {
+        String[] keywords = { "Good", "Bad", "Ugly" };
 
         Seminar mysem = new Seminar(1729, "Seminar Title", "2405231000", 75,
             (short)15, (short)33, 125, keywords, "This is a great seminar");
         String semPrint = mysem.toString();
         System.out.println(semPrint);
-        assertTrue(semPrint.equals("ID: 1729, Title: Seminar Title\n" +
-            "Date: 2405231000, Length: 75, X: 15, Y: 33, Cost: 125\n" +
-            "Description: This is a great seminar\n" +
-            "Keywords: Good, Bad, Ugly"));
+        assertTrue(semPrint.equals("ID: 1729, Title: Seminar Title\n"
+            + "Date: 2405231000, Length: 75, X: 15, Y: 33, Cost: 125\n"
+            + "Description: This is a great seminar\n"
+            + "Keywords: Good, Bad, Ugly"));
     }
 }
