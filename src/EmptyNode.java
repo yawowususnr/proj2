22329e23f5d91d7300aec707ac4eb768eff1c67f
Yaw Owusu Jnr <yawjnr@vt.edu>
2024-10-08 02:30:59 -0400

    Update EmptyNode.java
    Added the new class outline

--- src/EmptyNode.java
+++ src/EmptyNode.java
@@ -1,4 +1,46 @@
+/**
+ 
+A modified node class for empty nodes using flyweight design patterns
+@author Elishah Steele (esteele21)
+@author Clarence Braimah (clarencebraimah)
+@version 10/9/23
 
-public class EmptyNode {
+ **/
+class EmptyNode implements BintreeNode {
+    private static EmptyNode instance; // the empty node instance
 
+
+  private EmptyNode() {}
+
+
+public static EmptyNode getInstance() {
+    if (instance == null) {
+        instance = new EmptyNode();}
+    return instance;}
+
+
+
+     
+
+
+public boolean isInternal() {
+    return false;}
+
+
+     
+
+public boolean isLeaf() {
+    return false;}
+
+
+
+
+public BintreeNode insert(Seminar seminar, int level, BoundingBox bbox) {// Replace the empty node with a leaf node
+    return new LeafNode(seminar);}
+
+
+
+public BintreeNode remove(Seminar seminar, int level, BoundingBox bbox) {
+    return EmptyNode.getInstance();
+    }
 }
