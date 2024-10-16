9c2df119787f7d811848c22b3ada0973cd6e3f4d
chrisnicoue <144281691+chrisnicoue@users.noreply.github.com>
2024-10-16 13:54:54 -0400

    Cotrller test

--- src/ControllerTest.java
+++ src/ControllerTest.java
@@ -14,12 +14,18 @@ public class ControllerTest extends TestCase {
 
     public void testInsert() {
         assertEquals(this.controller.getSize(), 100);
+        KeyValuePair<Integer, Seminar> foundNode = controller.getidBST.find(5);
         String[] keywords = { "Java", "Programming" };
         controller.insert(1, "Java Seminar", "2024-10-01", 60, (short)10,
             (short)20, 500, keywords, keywords.length, "Learn Java");
         assertEquals(this.controller.getidBSTree().size(), 1);
         assertNotNull(controller.getcostBSTree().find(500));
         assertNotNull(controller.getdateBSTree().find("2024-10-01"));
+        controller.insert(1, "Java Seminar", "2024-10-01", 60, (short)210,
+            (short)220, 500, keywords, keywords.length, "Learn Java");
+        assertNull(foundNode);
+        assertFalse(controller.checkIfValid((short)210, (short)220));
+        
    
     }
 
